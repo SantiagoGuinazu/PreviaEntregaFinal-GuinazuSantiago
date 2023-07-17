fdae53df02b9c655264c88d89ffee640e0fc9d33
Santiago Guinazu <santigui2003@gmail.com>
2023-07-17 15:05:33 -0300

    then catch

--- script.js
+++ script.js
@@ -12,91 +12,64 @@ miProgramaPrincipal()
 //Funcion Inicio miPromgramaPrincipal
 function miProgramaPrincipal() {
     
-    //DB Usuarios
-    let usuarios = [
-        { usuario: "santigui2003", id: 100, nombre: "Santiago", apellido: "Guiñazu", foto: "usuario1.png"},
-        { usuario: "gustagui2003", id: 101, nombre: "Gustavo", apellido: "Guiñazu", foto: "usuario2.png"},
-        { usuario: "pauligui2003", id: 102, nombre: "Paula", apellido: "Guiñazu", foto: "usuario3.png"},
-    ]
-
-    //DB Productos
-    let productos = [
-        { id: 1, nombre: "predator", marca: "adidas", color: "blanco", tapones: "bajos", stock: 4, precio: 15000, rutaImagen: "botin1.png" },
-        { id: 2, nombre: "copa", marca: "adidas", color: "negro", tapones: "intermedios", stock: 1, precio: 26000, rutaImagen: "botin2.png" },
-        { id: 3, nombre: "mercurial", marca: "nike", color: "negro", tapones: "bajos", stock: 7, precio: 22000, rutaImagen: "botin3.png" },
-        { id: 4, nombre: "vapor", marca: "nike", color: "blanco", tapones: "intermedios", stock: 5, precio: 8000, rutaImagen: "botin4.png" },
-        { id: 5, nombre: "tiempo", marca: "nike", color: "blanco", tapones: "bajos", stock: 1, precio: 8000, rutaImagen: "botin5.png" },
-        { id: 6, nombre: "x", marca: "adidas", color: "negro", tapones: "altos", stock: 8, precio: 7500, rutaImagen: "botin6.png" },
-        { id: 7, nombre: "copa pro", marca: "adidas", color: "blanco", tapones: "bajos", stock: 4, precio: 3500, rutaImagen: "botin7.png" },
-        { id: 8, nombre: "phantom", marca: "nike", color: "negro", tapones: "altos", stock: 5, precio: 20000, rutaImagen: "botin8.png" },
-        { id: 9, nombre: "pro copa", marca: "adidas", color: "blanco", tapones: "intermedios", stock: 9, precio: 17000, rutaImagen: "botin9.png" },
-        { id: 10, nombre: "superfly", marca: "nike", color: "negro", tapones: "altos", stock: 2, precio: 9600, rutaImagen: "botin10.png" },
-        { id: 11, nombre: "speedflow", marca: "adidas", color: "blanco", tapones: "intermedios", stock: 10, precio: 17600, rutaImagen: "botin11.png" },
-        { id: 12, nombre: "phantom", marca: "nike", color: "negro", tapones: "altos", stock: 3, precio: 12300, rutaImagen: "botin12.png" },
-        { id: 13, nombre: "superfly", marca: "nike", color: "blanco", tapones: "bajos", stock: 5, precio: 29000, rutaImagen: "botin13.png" },
-        { id: 14, nombre: "ghosted", marca: "adidas", color: "blanco", tapones: "altos", stock: 2, precio: 35000, rutaImagen: "botin14.png" },
-        { id: 15, nombre: "mercurial", marca: "nike", color: "negro", tapones: "intermedios", stock: 15, precio: 17000, rutaImagen: "botin15.png" },
-        { id: 16, nombre: "vapor", marca: "nike", color: "negro", tapones: "bajos", stock: 7, precio: 15200, rutaImagen: "botin16.png" },
-    ]
-
     //Contenedor productos
     let contenedor = document.getElementById("productos")
-
     //1 - Ingreso Usuario
     let botonLogin = document.getElementById("botonLogin")
-    botonLogin.addEventListener("click", () => mostrarBienvenida(usuarios))
-
     //2 - Lista productos
     let botonProductos = document.getElementById("botonProductos")
-    botonProductos.addEventListener("click", (e) => listaCompleta(e, productos, contenedor))
-
     //3 - Filtro para Adidas y Nike
     let botonesFiltros = document.getElementsByClassName("menuFiltro")
-    for (let botonFiltro of botonesFiltros) {
-        botonFiltro.addEventListener("click", (e) => filtrarYRenderizarPorMarca(e, productos, contenedor))
-    }
-
     //4 - Buscador general
     let buscador = document.getElementById("buscador")
-    buscador.addEventListener("input", () => filtrar(productos, buscador, contenedor))
-
     //5 - Tarjetas para carrito
-    crearTarjetas(productos, contenedor)
-
     //6 - Agregar al Carrito - Funcion Interna
-
     //7 - Renderizar Carrito
-    renderizarCarrito()
-
     //8 - Boton Carrito
     let botonCarrito = document.getElementById("botonCarrito")
-    botonCarrito.addEventListener("click", mostrarOcultar)
-
     //9 - Boton Lista productos e Inicio
     let botonListaBotines = document.getElementById("botonProductos")
-    botonListaBotines.addEventListener("click", mostrarOcultarLista)
-
     //10 - Finalizar Compra
     let botonFinalizarCompra = document.getElementById("finalizarCompra")
-    botonFinalizarCompra.addEventListener("click", () => finalizarCompra())
 
-    //11 - Funcion Interna SweetAlerts
-    
-    //12 - Funcion Interna Toastify
-
-    /*
-    PROXIMO PASO DATAJSON + FETCH THEN CATCH
+    //DataBase JSON
     dbJSON()
 
+    //Funcion DataBase JSON
     function dbJSON() {
         let dbJSON = "./db.json"
 
-        fetch(dataJSON)
-            .then
-            .then 
-            .catch
+        fetch(dbJSON)
+            .then((response) => response.json())
+            .then((data) => {
+                let usuarios = data.usuarios
+                let productos = data.productos
+
+                    //1 - Ingreso Usuario
+                    botonLogin.addEventListener("click", () => mostrarBienvenida(usuarios))
+                    //2 - Lista productos
+                    botonProductos.addEventListener("click", (e) => listaCompleta(e, productos, contenedor))
+                    //3 - Filtro para Adidas y Nike
+                    for (let botonFiltro of botonesFiltros) {
+                        botonFiltro.addEventListener("click", (e) => filtrarYRenderizarPorMarca(e, productos, contenedor))
+                    }
+                    //4 - Buscador general
+                    buscador.addEventListener("input", () => filtrar(productos, buscador, contenedor))
+                    //5 - Tarjetas para carrito
+                    crearTarjetas(productos, contenedor)
+                    //6 - Agregar al Carrito - Funcion Interna
+                    //7 - Renderizar Carrito
+                    renderizarCarrito()
+                    //8 - Boton Carrito
+                    botonCarrito.addEventListener("click", mostrarOcultar)
+                    //9 - Boton Lista productos e Inicio
+                    botonListaBotines.addEventListener("click", mostrarOcultarLista)
+                    //10 - Finalizar Compra
+                    botonFinalizarCompra.addEventListener("click", () => finalizarCompra())
+                    //11 - Funcion Interna SweetAlerts
+                    //12 - Funcion Interna Toastify
+            })
     }
-    */
 }
 
 
@@ -120,7 +93,6 @@ function mostrarBienvenida(arrayIngresado) {
     }
 }
 
-
 //2 - Lista productos
 function listaCompleta(e, arrayProductos, contenedor) {
     e.preventDefault()
@@ -130,20 +102,21 @@ function listaCompleta(e, arrayProductos, contenedor) {
     }
 }
 
-
 //3 - Filtro para Adidas y Nike
 function filtrarYRenderizarPorMarca(e, arrayProductos, contenedor) {
     let elementosFiltrados = arrayProductos.filter(producto => producto.marca === e.target.value)
+    let inicio = document.getElementById("inicio")
+    inicio.classList.add("oculto")
     crearTarjetas(elementosFiltrados, contenedor)
 }
-
 
 //4 - Buscador general
 function filtrar(arrayProductos, input, contenedor) {
     let arrayFiltrado = arrayProductos.filter(producto => producto.nombre.includes(input.value.toLowerCase()) || producto.marca.includes(input.value.toLowerCase()))
+    let inicio = document.getElementById("inicio")
+    inicio.classList.add("oculto")
     crearTarjetas(arrayFiltrado, contenedor)
 }
-
 
 //5 - Tarjetas para carrito
 function crearTarjetas(arrayProductos, contenedor) {
@@ -168,7 +141,6 @@ function crearTarjetas(arrayProductos, contenedor) {
     })
 }
 
-
 //6 - Agregar a Carrito
 function agregarAlCarrito(arrayProductos, id) {
     let productoBuscado = arrayProductos.find(producto => producto.id === id)
@@ -192,7 +164,6 @@ function agregarAlCarrito(arrayProductos, id) {
     renderizarCarrito()
     lanzarTostada()
 }
-
 
 //7 - Renderizar Carrito
 function renderizarCarrito() {
@@ -229,7 +200,6 @@ function renderizarCarrito() {
     carritoTotal.append(compraParcial)
 }
 
-
 //8 - Boton Carrito
 function mostrarOcultar() {
     let contenedorCarrito = document.getElementById("contenedorCarrito")
@@ -238,13 +208,11 @@ function mostrarOcultar() {
     inicio.classList.add("oculto")
 }
 
-
 //9 - Boton Lista productos e Inicio
 function mostrarOcultarLista() {
     let inicio = document.getElementById("inicio")
     inicio.classList.add("oculto")
 }
-
 
 //10 - Finalizar Compra
 function finalizarCompra() {
@@ -259,7 +227,6 @@ function finalizarCompra() {
     carrito = [] 
     renderizarCarrito([])
 }
-
 
 //11 - SweetAlerts
 function lanzarAlert() {
@@ -278,7 +245,6 @@ function lanzarAlert2() {
         footer: '<a href="index.html">Ir a comprar?</a>',
     })
 }
-
 
 //12 - Toastify
 function lanzarTostada() {
